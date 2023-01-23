5d5b2d4237be71d5488520e30690b17bd4c8bd12
Robert Medina <csoperativo@gmail.com>
2023-01-23 00:52:15 -0400

    preparacion de la pagina

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from '@logos/DW3.png'
+
+
+const Nav = () => {
+  return (
+    <>
+    <ul>
+        <img src={logo} alt="logo" />
+        <li>Home</li>
+        <li>Mission</li>
+        <li>Team</li>
+    </ul>
+    </>
+      )
+}
+
+export default Nav
